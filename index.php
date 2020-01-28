07eb4670744cff57f9b1cfd56b262a8777465204
emavale24 <60410103+emavale24@users.noreply.github.com>
2020-01-28 23:18:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+
+
+	$partenza = $json->queryResult->parameters->partenza; 
+	$arrivo = $json->queryResult->parameters->arrivo; 
+
+//ignorare 
+
+if($partenza!=null $$ $arrivo!=null)
+{
+	if($partenza=="trani" && $arrivo=="bari");
+{
+	$speech = "parte alle 10 e arriva alle 11";
+}
+	if($partenza=="corato" && $arrivo=="trani");
+{
+
+	$speech = "parte alle 9 e arriva alle 10.30";
+
+}  
+	$speech = "parte alle 9 e arriva alle 10.30";
+
+}
+else {
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
